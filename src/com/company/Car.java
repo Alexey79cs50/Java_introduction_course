ff06c2379ae2cbe54b1ae7bfa858bdda8a9bc4f0
Alexey79cs50 <55100567+Alexey79cs50@users.noreply.github.com>
2020-02-03 01:05:16 +0200

    Add files via upload

---
+++ src/com/company/Car.java
@@ -0,0 +1,18 @@
+package com.company;
+
+//class definition
+public class Car {
+    String orderID, brand, model, colour, regNum;
+    int year;
+
+    //constructor
+    Car(String orderID, String brand, String model, String colour, String regNum, int year ){
+        this.orderID = orderID;
+        this.brand = brand;
+        this.model = model;
+        this.colour = colour;
+        this.regNum = regNum;
+        this.year = year;
+    }
+}
+
